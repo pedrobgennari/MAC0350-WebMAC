5b3ea4398d4c629e655c8992fda467eb795821a6
Pedro Barletta Gennari <pedro.pbg@usp.br>
2026-03-30 12:42:26 -0300

    add 'aula06/ex_sala'

---
+++ exs/aula06/ex_sala/bd.sql
@@ -0,0 +1,58 @@
+CREATE TABLE entregador (
+    entregador_id INTEGER PRIMARY KEY,
+  	nome TEXT,
+  	contato TEXT
+);
+
+CREATE TABLE endereco (
+    endereco_id INTEGER PRIMARY KEY,
+    rua TEXT NOT NULL,
+    numero TEXT NOT NULL,
+    bairro TEXT NOT NULL,
+    cidade TEXT NOT NULL,
+    cep TEXT NOT NULL
+);
+
+CREATE TABLE cliente (
+    cliente_id INTEGER PRIMARY KEY,
+  	nome TEXT,
+  	contato TEXT,
+  	endereco_id INTEGER,
+  
+  	FOREIGN KEY (endereco_id) REFERENCES endereco(endereco_id)
+);
+
+CREATE TABLE vendedor (
+    vendedor_id INTEGER PRIMARY KEY,
+  	nome TEXT,
+  	contato TEXT
+);
+
+CREATE TABLE fabricante (
+    fabricante_id INTEGER PRIMARY KEY,
+  	nome TEXT
+);
+
+CREATE TABLE produto (
+    produto_id INTEGER PRIMARY KEY,
+  	nome TEXT,
+  	fabricante_id INTEGER,
+  
+  	FOREIGN KEY (fabricante_id) REFERENCES fabricante(fabricante_id)
+);
+
+CREATE TABLE pedido (
+    pedido_id INTEGER PRIMARY KEY,
+    
+    status_entrega TEXT NOT NULL,
+    
+    entregador_id INTEGER NOT NULL,
+    cliente_id INTEGER NOT NULL,
+    vendedor_id INTEGER NOT NULL,
+    produto_id INTEGER NOT NULL,
+
+    FOREIGN KEY (entregador_id) REFERENCES entregador(entregador_id),
+    FOREIGN KEY (cliente_id) REFERENCES cliente(cliente_id),
+    FOREIGN KEY (vendedor_id) REFERENCES vendedor(vendedor_id),
+    FOREIGN KEY (produto_id) REFERENCES produto(produto_id)
+);
